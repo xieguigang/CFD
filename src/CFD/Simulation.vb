77ea4960d7a06f4902d2bff59cfb2edeada7f6ae
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-11-30 18:36:53 +0800

    run package msbuild

--- src/CFD/Simulation.vb
+++ src/CFD/Simulation.vb
@@ -6,34 +6,15 @@ Imports Microsoft.VisualBasic.My.JavaScript
 Public MustInherit Class Simulation : Implements requestAnimationFrame
 
     ' *************************************************************************
-    '                                - DIMENTIONS -                            *
-    ' **************************************************************************
-
-    ' simulation canvas size
-    Friend Shared width As Integer = 1200, height As Integer = 480
+    '                           - DIMENTIONS -         -----                  *
+    ' *************************************************************************
     ' number of data points / pixels per dimention
-    Friend xdim As Integer = 1920, ydim As Integer = 1080 ' HD
-    ' static int xdim = 2400, ydim = 960;
-    ' static int xdim = 1200, ydim = 480;
-    ' static int xdim = 600, ydim = 240;
-    ' static int xdim = 400, ydim = 160;
-    ' static int xdim = 200, ydim = 80;
-    ' static int xdim = 100, ydim = 40;
+    Friend xdim As Integer = 1920
+    Friend ydim As Integer = 1080
 
-    Public Overridable Sub setDimentions(width As Integer, height As Integer, xdim As Integer, ydim As Integer)
-        ' StdDraw.setCanvasSize(width, height)
-
-        Me.xdim = xdim
-        Me.ydim = ydim
-
-        ' Set the drawing scale to dimentions
-        ' the -.5 is so that the coordinates align with the center of the pixel
-        ' StdDraw.setXscale(0 - .5, xdim - .5)
-        ' StdDraw.setYscale(0 - .5, ydim - .5)
-
-        ' Set 1px pen radius
-        Dim r = 1.0 / width
-        ' StdDraw.PenRadius = r
+    Public Overridable Sub setDimentions(width As Integer, height As Integer)
+        xdim = width
+        ydim = height
     End Sub
 
     ''' <summary>
@@ -41,7 +22,6 @@ Public MustInherit Class Simulation : Implements requestAnimationFrame
     ''' METHODS                                                                  *
     ''' **************************************************************************
     ''' </summary>
-
     Public MustOverride Sub reset()
     Public MustOverride Sub advance()
     Protected MustOverride Sub draw(g As IGraphics) Implements requestAnimationFrame.requestAnimationFrame
@@ -57,18 +37,18 @@ Public Class AnimationBuilder
     Public Property xdim As Integer = 1920
     Public Property ydim As Integer = 1080
 
-    Friend frameDelay As Integer = 30
-    Friend timeStepsPerFrame As Integer = 1
-    Friend screenshotRate As Integer = 250
-    Friend shouldTakeScreenshots As Boolean = True
-    Friend screenshotName As String = "Screenshot"
+    Public Property frameDelay As Integer = 30
+    Public Property timeStepsPerFrame As Integer = 1
+    Public Property screenshotRate As Integer = 250
+    Public Property shouldTakeScreenshots As Boolean = True
+    Public Property screenshotName As String = "Screenshot"
 
     Dim time As Integer = 0
     Dim i As Integer = 0
 
     Sub Run(source As Simulation)
         Call source.reset()
-        Call source.setDimentions(0, 0, xdim, ydim)
+        Call source.setDimentions(xdim, ydim)
 
         Do While True
             If time Mod screenshotRate = 0 AndAlso shouldTakeScreenshots Then
