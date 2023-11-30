43ba9f4b9c988d95df8bd8de81766e731f823301
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-11-30 18:32:41 +0800

    Namespace Tasks

--- src/CFD/Tasks/bounce.vb
+++ src/CFD/Tasks/bounce.vb
@@ -1,52 +1,55 @@
 ﻿Imports Microsoft.VisualBasic.Parallel
 
-Public Class bounce : Inherits VectorTask
+Namespace Tasks
 
-    ReadOnly cfd As FluidDynamics
+    Public Class bounce : Inherits VectorTask
 
-    Sub New(cfd As FluidDynamics)
-        MyBase.New(nsize:=cfd.xdim)
-        Me.cfd = cfd
-    End Sub
+        ReadOnly cfd As FluidDynamics
 
-    Protected Overrides Sub Solve(start As Integer, ends As Integer, cpu_id As Integer)
-        For x As Integer = start To ends - 1
-            For Y As Integer = 0 To cfd.ydim - 1
-                If cfd.barrier(x)(Y) Then
-                    If cfd.nN(x)(Y) > 0 Then
-                        cfd.nS(x)(Y - 1) += cfd.nN(x)(Y)
-                        cfd.nN(x)(Y) = 0
+        Sub New(cfd As FluidDynamics)
+            MyBase.New(nsize:=cfd.xdim)
+            Me.cfd = cfd
+        End Sub
+
+        Protected Overrides Sub Solve(start As Integer, ends As Integer, cpu_id As Integer)
+            For x As Integer = start To ends - 1
+                For Y As Integer = 0 To cfd.ydim - 1
+                    If cfd.barrier(x)(Y) Then
+                        If cfd.nN(x)(Y) > 0 Then
+                            cfd.nS(x)(Y - 1) += cfd.nN(x)(Y)
+                            cfd.nN(x)(Y) = 0
+                        End If
+                        If cfd.nS(x)(Y) > 0 Then
+                            cfd.nN(x)(Y + 1) += cfd.nS(x)(Y)
+                            cfd.nS(x)(Y) = 0
+                        End If
+                        If cfd.nE(x)(Y) > 0 Then
+                            cfd.nW(x - 1)(Y) += cfd.nE(x)(Y)
+                            cfd.nE(x)(Y) = 0
+                        End If
+                        If cfd.nW(x)(Y) > 0 Then
+                            cfd.nE(x + 1)(Y) += cfd.nW(x)(Y)
+                            cfd.nW(x)(Y) = 0
+                        End If
+                        If cfd.nNW(x)(Y) > 0 Then
+                            cfd.nSE(x + 1)(Y - 1) += cfd.nNW(x)(Y)
+                            cfd.nNW(x)(Y) = 0
+                        End If
+                        If cfd.nNE(x)(Y) > 0 Then
+                            cfd.nSW(x - 1)(Y - 1) += cfd.nNE(x)(Y)
+                            cfd.nNE(x)(Y) = 0
+                        End If
+                        If cfd.nSW(x)(Y) > 0 Then
+                            cfd.nNE(x + 1)(Y + 1) += cfd.nSW(x)(Y)
+                            cfd.nSW(x)(Y) = 0
+                        End If
+                        If cfd.nSE(x)(Y) > 0 Then
+                            cfd.nNW(x - 1)(Y + 1) += cfd.nSE(x)(Y)
+                            cfd.nSE(x)(Y) = 0
+                        End If
                     End If
-                    If cfd.nS(x)(Y) > 0 Then
-                        cfd.nN(x)(Y + 1) += cfd.nS(x)(Y)
-                        cfd.nS(x)(Y) = 0
-                    End If
-                    If cfd.nE(x)(Y) > 0 Then
-                        cfd.nW(x - 1)(Y) += cfd.nE(x)(Y)
-                        cfd.nE(x)(Y) = 0
-                    End If
-                    If cfd.nW(x)(Y) > 0 Then
-                        cfd.nE(x + 1)(Y) += cfd.nW(x)(Y)
-                        cfd.nW(x)(Y) = 0
-                    End If
-                    If cfd.nNW(x)(Y) > 0 Then
-                        cfd.nSE(x + 1)(Y - 1) += cfd.nNW(x)(Y)
-                        cfd.nNW(x)(Y) = 0
-                    End If
-                    If cfd.nNE(x)(Y) > 0 Then
-                        cfd.nSW(x - 1)(Y - 1) += cfd.nNE(x)(Y)
-                        cfd.nNE(x)(Y) = 0
-                    End If
-                    If cfd.nSW(x)(Y) > 0 Then
-                        cfd.nNE(x + 1)(Y + 1) += cfd.nSW(x)(Y)
-                        cfd.nSW(x)(Y) = 0
-                    End If
-                    If cfd.nSE(x)(Y) > 0 Then
-                        cfd.nNW(x - 1)(Y + 1) += cfd.nSE(x)(Y)
-                        cfd.nSE(x)(Y) = 0
-                    End If
-                End If
+                Next
             Next
-        Next
-    End Sub
-End Class
+        End Sub
+    End Class
+End Namespace
