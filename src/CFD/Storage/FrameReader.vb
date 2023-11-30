956d6b66f1a418d86f14e75dc7947e3b7de80805
guigang xie <xie.guigang@gcmodeller.org>
2023-11-30 10:01:22 +0800

    read a frame data

--- src/CFD/Storage/FrameReader.vb
+++ src/CFD/Storage/FrameReader.vb
@@ -1,15 +1,34 @@
-﻿Imports System.IO
+﻿Imports System.Drawing
+Imports System.IO
+Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.Data.IO
 Imports Microsoft.VisualBasic.DataStorage.HDSPack.FileSystem
 
 Public Class FrameReader : Implements IDisposable
 
     ReadOnly buf As StreamPack
+    ReadOnly dims As Size
 
     Private disposedValue As Boolean
 
     Sub New(file As Stream)
         buf = New StreamPack(file, [readonly]:=True)
     End Sub
+
+    Public Function ReadFrame(i As Integer) As Double()()
+        Dim path As String = $"/frame_data/{i}.dat"
+        Dim file As Stream = buf.OpenFile(path, FileMode.Open, FileAccess.Read)
+        Dim rd As New BinaryDataReader(file) With {
+            .ByteOrder = ByteOrder.BigEndian
+        }
+        Dim framedata As Double()() = RectangularArray.Matrix(Of Double)(dims.Width, dims.Height)
+
+        For offset As Integer = 0 To framedata.Length - 1
+            framedata(offset) = rd.ReadDoubles(count:=dims.Height)
+        Next
+
+        Return framedata
+    End Function
 
     Protected Overridable Sub Dispose(disposing As Boolean)
         If Not disposedValue Then
