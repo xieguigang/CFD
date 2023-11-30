034b70eb3948940c2a885cf7c609d67aaf8b03d1
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-11-30 23:07:33 +0800

    Update small_demo.R

--- test/small_demo.R
+++ test/small_demo.R
@@ -1,8 +1,8 @@
 require(CFD);
 
 const file = CFD::open.pack(`${@dir}/demo.dat`, mode = "write");
-const dynamics = CFD::session(file);
+const dynamics = CFD::session(file, interval = 90);
 
 # run
-CFD::start(dynamics, max.time = 20000);
+CFD::start(dynamics, max.time = 100000);
 close(file);
