a4945fbf6c1f238c1b646e67550cc4ca9976e676
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-11-30 22:04:24 +0800

    run plot test success

--- test/read_debug.R
+++ test/read_debug.R
@@ -2,10 +2,10 @@ require(CFD);
 require(graphics2D);
 
 const file = CFD::open.pack(`${@dir}/demo.dat`, mode = "read");
-const frame = CFD::read.frameRaster(file, time = 100);
+const frame = CFD::read.frameRaster(file, time = 600);
 
-bitmap(file = `${@dir}/frame_demo.png`);
-rasterHeatmap(frame, colorName = "turbo", colorLevels = 20);
+bitmap(file = `${@dir}/frame_demo.png`, size = [1920,1080]);
+rasterHeatmap(frame, colorName = "viridis", colorLevels = 255);
 dev.off();
 
 close(file);
