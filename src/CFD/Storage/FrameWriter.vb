cb1badaa38ea37976dc61e6702af41288b649e04
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-11-30 20:14:56 +0800

    save data ranges

--- src/CFD/Storage/FrameWriter.vb
+++ src/CFD/Storage/FrameWriter.vb
@@ -1,8 +1,11 @@
 Imports System.Drawing
 Imports System.IO
+Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
 Imports Microsoft.VisualBasic.Data.IO
 Imports Microsoft.VisualBasic.DataStorage.HDSPack
 Imports Microsoft.VisualBasic.DataStorage.HDSPack.FileSystem
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports Microsoft.VisualBasic.Text
 
@@ -17,7 +20,7 @@ Namespace Storage
         ''' </summary>
         Dim total As Integer
         Dim dims As Integer()
-        Dim dimensions As New Dictionary(Of String, String)
+        Dim dimensions As New Dictionary(Of String, DoubleRange)
 
         Dim disposedValue As Boolean
 
@@ -40,8 +43,23 @@ Namespace Storage
                 Call wr.Write(row)
             Next
 
+            If Not dimensions.ContainsKey(dimension) Then
+                dimensions.Add(dimension, New DoubleRange(0, 0))
+            End If
+
+            Dim range As DoubleRange = (From row As Double() In framedata Select New DoubleRange(row)) _
+                .Select(Function(a) {a.Min, a.Max}) _
+                .IteratesALL _
+                .Range
+
             total += 1
-            dimensions(dimension) = dimension
+
+            If dimensions(dimension).Min > range.Min Then
+                dimensions(dimension).Min = range.Min
+            End If
+            If dimensions(dimension).Max < range.Max Then
+                dimensions(dimension).Max = range.Max
+            End If
 
             Call wr.Flush()
             Call file.Flush()
@@ -58,8 +76,14 @@ Namespace Storage
                 GetType(Integer), GetType(Integer()),
                 GetType(String), GetType(String())
             })
+            Dim ranges As Dictionary(Of String, Double()) = dimensions _
+                .ToDictionary(Function(d) d.Key,
+                              Function(d)
+                                  Return New Double() {d.Value.Min, d.Value.Max}
+                              End Function)
 
             Call buf.WriteText(json, fileName:="/metadata.json", Encodings.ASCII)
+            Call buf.WriteText(ranges.GetJson, fileName:="/ranges.json", Encodings.ASCII)
         End Sub
 
         Protected Overridable Sub Dispose(disposing As Boolean)
