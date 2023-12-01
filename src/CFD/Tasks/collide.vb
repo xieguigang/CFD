18ea754ca1ec999ef22a65d9587fae9b35d83ae2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-01 22:22:46 +0800

    symbol renames

--- src/CFD/Tasks/collide.vb
+++ src/CFD/Tasks/collide.vb
@@ -12,24 +12,25 @@ Namespace Tasks
         End Sub
 
         Protected Overrides Sub Solve(start As Integer, ends As Integer, cpu_id As Integer)
-            Dim n, one9thn, one36thn, vx, vy, vx2, vy2, vx3, vy3, vxvy2, v2, v215 As Double
+            Dim this_rho, one9thn, one36thn, vx, vy, vx2, vy2, vx3, vy3, vxvy2, v2, v215 As Double
             Dim omega = 1 / (3 * cfd.viscocity + 0.5) ' reciprocal of tau, the relaxation time
 
             For x As Integer = start To ends
                 For y As Integer = 0 To cfd.ydim - 1
                     If Not cfd.barrier(x)(y) Then
-                        n = cfd.n0(x)(y) + cfd.nN(x)(y) + cfd.nS(x)(y) + cfd.nE(x)(y) + cfd.nW(x)(y) + cfd.nNW(x)(y) + cfd.nNE(x)(y) + cfd.nSW(x)(y) + cfd.nSE(x)(y)
-                        cfd.rho(x)(y) = n ' macroscopic density may be needed for plotting
-                        one9thn = one9th * n
-                        one36thn = one36th * n
-                        If n > 0 Then
-                            vx = (cfd.nE(x)(y) + cfd.nNE(x)(y) + cfd.nSE(x)(y) - cfd.nW(x)(y) - cfd.nNW(x)(y) - cfd.nSW(x)(y)) / n
+                        this_rho = cfd.n0(x)(y) + cfd.nN(x)(y) + cfd.nS(x)(y) + cfd.nE(x)(y) +
+                            cfd.nW(x)(y) + cfd.nNW(x)(y) + cfd.nNE(x)(y) + cfd.nSW(x)(y) + cfd.nSE(x)(y)
+                        cfd.rho(x)(y) = this_rho ' macroscopic density may be needed for plotting
+                        one9thn = one9th * this_rho
+                        one36thn = one36th * this_rho
+                        If this_rho > 0 Then
+                            vx = (cfd.nE(x)(y) + cfd.nNE(x)(y) + cfd.nSE(x)(y) - cfd.nW(x)(y) - cfd.nNW(x)(y) - cfd.nSW(x)(y)) / this_rho
                         Else
                             vx = 0
                         End If
                         cfd.xvel(x)(y) = vx ' may be needed for plotting
-                        If n > 0 Then
-                            vy = (cfd.nN(x)(y) + cfd.nNE(x)(y) + cfd.nNW(x)(y) - cfd.nS(x)(y) - cfd.nSE(x)(y) - cfd.nSW(x)(y)) / n
+                        If this_rho > 0 Then
+                            vy = (cfd.nN(x)(y) + cfd.nNE(x)(y) + cfd.nNW(x)(y) - cfd.nS(x)(y) - cfd.nSE(x)(y) - cfd.nSW(x)(y)) / this_rho
                         Else
                             vy = 0
                         End If
@@ -42,7 +43,7 @@ Namespace Tasks
                         v2 = vx2 + vy2
                         cfd.speed2(x)(y) = v2 ' may be needed for plotting
                         v215 = 1.5 * v2
-                        cfd.n0(x)(y) += omega * (four9ths * n * (1 - v215) - cfd.n0(x)(y))
+                        cfd.n0(x)(y) += omega * (four9ths * this_rho * (1 - v215) - cfd.n0(x)(y))
                         cfd.nE(x)(y) += omega * (one9thn * (1 + vx3 + 4.5 * vx2 - v215) - cfd.nE(x)(y))
                         cfd.nW(x)(y) += omega * (one9thn * (1 - vx3 + 4.5 * vx2 - v215) - cfd.nW(x)(y))
                         cfd.nN(x)(y) += omega * (one9thn * (1 + vy3 + 4.5 * vy2 - v215) - cfd.nN(x)(y))
