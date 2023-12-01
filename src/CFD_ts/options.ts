f313af90a0970a3e72d6effc112a6bbe3f1cf452
guigang xie <xie.guigang@gcmodeller.org>
2023-12-01 14:39:04 +0800

    Update options.ts

--- src/CFD_ts/options.ts
+++ src/CFD_ts/options.ts
@@ -4,34 +4,43 @@ export const four9ths = 4.0 / 9.0;
 export const one9th = 1.0 / 9.0;
 export const one36th = 1.0 / 36.0;
 
+/**
+ * the simulation options
+*/
 export class options {
-    public stepCount = 0;
-    public startTime = 0;
-    public barrierCount = 0;
-    public barrierxSum = 0;
-    public barrierySum = 0;
-    public barrierFx = 0.0;						// total force on all barrier sites
-    public barrierFy = 0.0;
 
-    public mouseX;
-    public mouseY;							// mouse location in canvas coordinates
-    public oldMouseX = -1;
-    public oldMouseY = -1;			// mouse coordinates from previous simulation frame
-    public collectingData = false;
-    public time = 0;								// time (in simulation step units) since data collection started
-    public showingPeriod = false;
-    public lastBarrierFy = 1;						// for determining when F_y oscillation begins
-    public lastFyOscTime = 0;						// for calculating F_y oscillation period
-    public nTracers = 144;
+    /**
+     * create new simulation parameter set with default values
+    */
+    constructor(
+        public stepCount = 0,
+        public startTime = 0,
+        public barrierCount = 0,
+        public barrierxSum = 0,
+        public barrierySum = 0,
+        public barrierFx = 0.0,				// total force on all barrier sites
+        public barrierFy = 0.0,
 
-    public sensorX: number;						// coordinates of "sensor" to measure local fluid properties	
-    public sensorY: number;
+        public mouseX: number,
+        public mouseY: number,				// mouse location in canvas coordinates
+        public oldMouseX = -1,
+        public oldMouseY = -1,			    // mouse coordinates from previous simulation frame
+        public collectingData = false,
+        public time = 0,					// time (in simulation step units) since data collection started
+        public showingPeriod = false,
+        public lastBarrierFy = 1,			// for determining when F_y oscillation begins
+        public lastFyOscTime = 0,			// for calculating F_y oscillation period
+        public nTracers = 144,
 
-    public tracerX: number[];
-    public tracerY: number[];
+        public sensorX: number,				// coordinates of "sensor" to measure local fluid properties	
+        public sensorY: number,
 
-    public transBlackArraySize: number = 50;
-    public transBlackArray: string[];
+        public tracerX: number[],
+        public tracerY: number[],
+
+        public transBlackArraySize: number = 50,
+        public transBlackArray: string[]
+    ) { }
 }
 
 export interface uiAdapter {
