e1a1fa64d3bd350f2ea6acf71950838204ae7d64
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-02 14:07:43 +0800

    design the loader screen

--- win32_desktop/Forms/SplashScreen.Designer.vb
+++ win32_desktop/Forms/SplashScreen.Designer.vb
@@ -24,24 +24,82 @@ Partial Class SplashScreen
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(SplashScreen))
         PictureBox1 = New PictureBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' PictureBox1
         ' 
+        PictureBox1.BackColor = Color.Transparent
         PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), Image)
         PictureBox1.BackgroundImageLayout = ImageLayout.Zoom
-        PictureBox1.Location = New Point(22, 12)
+        PictureBox1.Location = New Point(15, 34)
         PictureBox1.Name = "PictureBox1"
-        PictureBox1.Size = New Size(142, 229)
+        PictureBox1.Size = New Size(113, 182)
         PictureBox1.TabIndex = 0
         PictureBox1.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Transparent
+        Label1.Font = New Font("Consolas", 36F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.ForeColor = Color.White
+        Label1.Location = New Point(139, 34)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(388, 56)
+        Label1.TabIndex = 1
+        Label1.Text = "Moira Workshop"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.Transparent
+        Label2.Font = New Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.ForeColor = Color.White
+        Label2.Location = New Point(145, 91)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(212, 20)
+        Label2.TabIndex = 2
+        Label2.Text = "Computational Fluid Dynamics"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = Color.Transparent
+        Label3.Font = New Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point)
+        Label3.ForeColor = Color.White
+        Label3.Location = New Point(12, 286)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(196, 34)
+        Label3.TabIndex = 3
+        Label3.Text = "This free software is licensed to " & vbCrLf & "%s under then MIT license"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.BackColor = Color.Transparent
+        Label4.ForeColor = Color.DeepSkyBlue
+        Label4.Location = New Point(311, 305)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(225, 15)
+        Label4.TabIndex = 4
+        Label4.Text = "©xieguigang.me 2024, all rights reserved."
         ' 
         ' SplashScreen
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(534, 329)
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(548, 329)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
         Controls.Add(PictureBox1)
         FormBorderStyle = FormBorderStyle.None
         Name = "SplashScreen"
@@ -51,7 +109,12 @@ Partial Class SplashScreen
         TopMost = True
         CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
     Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
 End Class
--- win32_desktop/Forms/SplashScreen.vb
+++ win32_desktop/Forms/SplashScreen.vb
@@ -1,3 +1,5 @@
 ﻿Public Class SplashScreen
-
+    Private Sub SplashScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label3.Text = Label3.Text.Replace("%s", Environment.UserName)
+    End Sub
 End Class
