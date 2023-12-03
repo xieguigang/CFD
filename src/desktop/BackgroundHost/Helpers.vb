94f0c8e86f82bb8bcb27768e1d4d22a7b16ef052
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-03 22:13:46 +0800

    log commandline

--- src/desktop/BackgroundHost/Helpers.vb
+++ src/desktop/BackgroundHost/Helpers.vb
@@ -1,3 +1,20 @@
-﻿Public Module Helpers
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Logging
 
+Public Module Helpers
+
+    ReadOnly logfile As String = $"{App.ProductSharedDIR}/moria_{Now.Year}{Now.Month.FormatZero}.log"
+
+    Public Sub logCommandLine(app As String, commandLine As String, workdir As String,
+                              <CallerMemberName>
+                              Optional trace As String = Nothing)
+
+        Dim log As New LogFile(Helpers.logfile, autoFlush:=True, append:=True)
+
+        Call log.info($"app={app}", trace)
+        Call log.info($"commandline={commandLine}", trace)
+        Call log.Debug("workdir=%s", workdir)
+        Call log.Debug("pipeline=""%s"" %s", app, commandLine)
+        Call log.Dispose()
+    End Sub
 End Module
