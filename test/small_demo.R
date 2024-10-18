cfec6f4b8be843db1716d5d51b16dfb97b525a5d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-18 20:06:57 +0800

    config demo test script

--- test/small_demo.R
+++ test/small_demo.R
@@ -1,8 +1,11 @@
 require(CFD);
 
-const file = CFD::open.pack(`${@dir}/demo.dat`, mode = "write");
+setwd(@dir);
+
+const file = CFD::open.pack("./demo.dat", mode = "write");
 const dynamics = CFD::session(file, dims = [800,500], 
-    interval = 90, model.file = "G:\CFD\src\desktop\demo_model.png");
+    interval = 90, 
+    model.file = "../src/desktop/demo_model.png");
 
 # run
 CFD::start(dynamics, max.time = 100000);
